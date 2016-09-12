ccecf5264ad1fdd251bdf9dca3537341b2a3ddd5
kwan <303198069@qq.com>
2016-09-13 04:17:19 +0800

    misc

--- src/ThinkPHP.php
+++ src/ThinkPHP.php
@@ -4,6 +4,7 @@
      * 公共入口文件
      */
 
+    defined('APP_PATH') or die('请在入口文件中定义常量APP_PATH');
     /*
      * 记录开始运行时间
      */
@@ -36,7 +37,6 @@
      * 系统常量定义
      */
     defined('THINK_PATH') or define('THINK_PATH', __DIR__ . '/');
-    defined('APP_PATH') or define('APP_PATH', dirname($_SERVER['SCRIPT_FILENAME']) . '/');
     defined('APP_STATUS') or define('APP_STATUS', null); // 应用状态 加载对应的配置文件
     defined('APP_DEBUG') or define('APP_DEBUG', false); // 是否调试模式
 
@@ -48,26 +48,26 @@
         defined('STORAGE_TYPE') or define('STORAGE_TYPE', 'File'); // 存储类型 默认为File
     }
 
-    defined('RUNTIME_PATH') or define('RUNTIME_PATH', APP_PATH . 'Runtime/');   // 系统运行时目录
-    defined('LIB_PATH') or define('LIB_PATH', __DIR__ . '/'); // 系统核心类库目录
     //@todo have effect on BuildLiteBehavior
-    defined('CORE_PATH') or define('CORE_PATH', LIB_PATH . 'Think/'); // Think类库目录
-    defined('BEHAVIOR_PATH') or define('BEHAVIOR_PATH', __DIR__ . '/Behavior/'); // 行为类库目录
+//    defined('CORE_PATH') or define('CORE_PATH', LIB_PATH . 'Think/'); // Think类库目录
+//    defined('BEHAVIOR_PATH') or define('BEHAVIOR_PATH', __DIR__ . '/Behavior/'); // 行为类库目录
     defined('MODE_PATH') or define('MODE_PATH', __DIR__ . '/Mode/'); // 系统应用模式目录
     defined('COMMON_PATH') or define('COMMON_PATH', APP_PATH . 'Common/'); // 应用公共目录
+    defined('RUNTIME_PATH') or define('RUNTIME_PATH', APP_PATH . 'Runtime/');   // 系统运行时目录
+    defined('ADDON_PATH') or define('ADDON_PATH', APP_PATH . 'Addon');
+    defined('HTML_PATH') or define('HTML_PATH', APP_PATH . 'Html/'); // 应用静态目录
     defined('CONF_PATH') or define('CONF_PATH', COMMON_PATH . 'Conf/'); // 应用配置目录
     defined('LANG_PATH') or define('LANG_PATH', COMMON_PATH . 'Lang/'); // 应用语言目录
-    defined('HTML_PATH') or define('HTML_PATH', APP_PATH . 'Html/'); // 应用静态目录
     defined('LOG_PATH') or define('LOG_PATH', RUNTIME_PATH . 'Logs/'); // 应用日志目录
     defined('TEMP_PATH') or define('TEMP_PATH', RUNTIME_PATH . 'Temp/'); // 应用缓存目录
     defined('DATA_PATH') or define('DATA_PATH', RUNTIME_PATH . 'Data/'); // 应用数据目录
     defined('CACHE_PATH') or define('CACHE_PATH', RUNTIME_PATH . 'Cache/'); // 应用模板缓存目录
     defined('CONF_EXT') or define('CONF_EXT', EXT); // 配置文件后缀
     defined('CONF_PARSE') or define('CONF_PARSE', '');    // 配置文件解析方法
-    defined('ADDON_PATH') or define('ADDON_PATH', APP_PATH . 'Addon');
     define('IS_CGI', (0 === strpos(PHP_SAPI, 'cgi') || false !== strpos(PHP_SAPI, 'fcgi')) ? 1 : 0);
-    define('IS_WIN', strstr(PHP_OS, 'WIN') ? 1 : 0);
+    define('IS_WIN', strpos(PHP_OS, 'WIN') ? 1 : 0);
     define('IS_CLI', PHP_SAPI == 'cli' ? 1 : 0);
+    defined('LIB_PATH') or define('LIB_PATH', __DIR__ . '/'); // 系统核心类库目录@todo remove in future
 
     if (!IS_CLI) {
         // 当前文件名
