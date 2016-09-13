041890b121babf591a58a63468281ece76a7877f
kwan <303198069@qq.com>
2016-09-13 23:16:41 +0800

    fix

--- src/Conf/tags.php
+++ src/Conf/tags.php
@@ -4,24 +4,22 @@
      */
 
     return [
-        'tags' => [
-            'app_init'        => [
+        'app_init'        => [
 //                \Behavior\BuildLiteBehavior::class, // 生成运行Lite文件
-            ],
-            'app_begin'       => [
-                \Behavior\ReadHtmlCacheBehavior::class, // 读取静态缓存
-            ],
-            'app_end'         => [
-                \Behavior\ShowPageTraceBehavior::class, // 页面Trace显示
-            ],
-            'view_parse'      => [
-                \Behavior\ParseTemplateBehavior::class, // 模板解析 支持PHP、内置模板引擎和第三方模板引擎
-            ],
-            'template_filter' => [
-                \Behavior\ContentReplaceBehavior::class, // 模板输出替换
-            ],
-            'view_filter'     => [
-                \Behavior\WriteHtmlCacheBehavior::class, // 写入静态缓存
-            ],
+        ],
+        'app_begin'       => [
+            \Behavior\ReadHtmlCacheBehavior::class, // 读取静态缓存
+        ],
+        'app_end'         => [
+            \Behavior\ShowPageTraceBehavior::class, // 页面Trace显示
+        ],
+        'view_parse'      => [
+            \Behavior\ParseTemplateBehavior::class, // 模板解析 支持PHP、内置模板引擎和第三方模板引擎
+        ],
+        'template_filter' => [
+            \Behavior\ContentReplaceBehavior::class, // 模板输出替换
+        ],
+        'view_filter'     => [
+            \Behavior\WriteHtmlCacheBehavior::class, // 写入静态缓存
         ],
     ];
