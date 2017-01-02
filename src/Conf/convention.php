9ffe95e2c0d85ef6cf9d07a11f4f5ca9e8d38fe2
kwan <303198069@qq.com>
2017-01-02 23:31:34 +0800

    remove Crypt related classes

--- src/Conf/convention.php
+++ src/Conf/convention.php
@@ -153,6 +153,5 @@
         'HTTP_CACHE_CONTROL' => 'private',  // 网页缓存控制
         'CHECK_APP_DIR'      => true,       // 是否检查应用目录是否创建
         'FILE_UPLOAD_TYPE'   => 'Local',    // 文件上传方式
-        'DATA_CRYPT_TYPE'    => 'Think',    // 数据加密方式
 
     ];
