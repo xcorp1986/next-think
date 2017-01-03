87d3d84b86897ef0f333170decb5b15b0fc0d2a0
kwan <303198069@qq.com>
2017-01-03 23:30:02 +0800

    misc

--- src/Conf/convention.php
+++ src/Conf/convention.php
@@ -89,9 +89,9 @@
 
         /* 模板引擎设置 */
         'TMPL_CONTENT_TYPE'      => 'text/html', // 默认模板输出类型
-        'TMPL_ACTION_ERROR'      => __DIR__ . '/../Tpl/dispatch_jump.tpl', // 默认错误跳转对应的模板文件
-        'TMPL_ACTION_SUCCESS'    => __DIR__ . '/../Tpl/dispatch_jump.tpl', // 默认成功跳转对应的模板文件
-        'TMPL_EXCEPTION_FILE'    => __DIR__ . '/../Tpl/think_exception.tpl',// 异常页面的模板文件
+        'TMPL_ACTION_ERROR'      => __DIR__ . '/../Resources/dispatch_jump.tpl', // 默认错误跳转对应的模板文件
+        'TMPL_ACTION_SUCCESS'    => __DIR__ . '/../Resources/dispatch_jump.tpl', // 默认成功跳转对应的模板文件
+        'TMPL_EXCEPTION_FILE'    => __DIR__ . '/../Resources/think_exception.tpl',// 异常页面的模板文件
         'TMPL_DETECT_THEME'      => false,       // 自动侦测模板主题
         'TMPL_TEMPLATE_SUFFIX'   => '.html',     // 默认模板文件后缀
         'TMPL_FILE_DEPR'         => '/', //模板文件CONTROLLER_NAME与ACTION_NAME之间的分割符
