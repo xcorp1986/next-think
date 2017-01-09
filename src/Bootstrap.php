ee60523ef6fedf4b6b9e68d6ba0e3790439f6a34
kwan <303198069@qq.com>
2017-01-10 00:50:07 +0800

    misc

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -25,8 +25,6 @@
     /*
      * URL 模式定义
      */
-//    const URL_COMMON = 0;  //普通模式
-//    const URL_PATHINFO = 1;  //PATHINFO模式
     const URL_REWRITE = 2;  //REWRITE模式
     const URL_COMPAT = 3;  // 兼容模式
     
