8d469c905beddba825c796c354524e855c43be8a
kwan <303198069@qq.com>
2017-01-10 03:44:33 +0800

    fix #9

--- src/Think/Controller.php
+++ src/Think/Controller.php
@@ -242,16 +242,17 @@
                     exit(xml_encode($data));
                 case 'JSONP':
                     // 返回JSON数据格式到客户端 包含状态信息
-                    header('Content-Type:application/json; charset=utf-8');
+                    header('Content-Type:text/javascript; charset=utf-8');
                     $handler = isset($_GET[C('VAR_JSONP_HANDLER')]) ? $_GET[C('VAR_JSONP_HANDLER')] : C('DEFAULT_JSONP_HANDLER');
                     exit($handler . '(' . json_encode($data, $json_option) . ');');
                 case 'EVAL' :
                     // 返回可执行的js脚本
-                    header('Content-Type:text/html; charset=utf-8');
+                    header('Content-Type:text/javascript; charset=utf-8');
                     exit($data);
                 default     :
-                    // 用于扩展其他返回格式数据
-                    Hook::listen('ajax_return', $data);
+                    break;
+                // 用于扩展其他返回格式数据
+//                    Hook::listen('ajax_return', $data);
             }
         }
         
