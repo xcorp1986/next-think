089660441ea9c359d268bd32cc55f2f54eefadce
kwan <303198069@qq.com>
2017-01-11 03:27:32 +0800

    misc

--- src/Conf/convention.php
+++ src/Conf/convention.php
@@ -7,6 +7,7 @@
      * 所有配置参数都可以在生效前动态改变
      */
     use Think\Template\TagLib\Cx;
+    use Think\Url\UrlCaseSensitivity;
     use Think\Url\UrlSchema;
     
     return [
@@ -126,7 +127,7 @@
         ],
 
         /* URL设置 */
-        'URL_CASE_INSENSITIVE'   => true,   // 默认false 表示URL区分大小写 true则表示不区分大小写
+        'URL_CASE_INSENSITIVE'   => UrlCaseSensitivity::SENSITIVITY,   // 默认false 表示URL区分大小写 true则表示不区分大小写
         'URL_MODEL'              => UrlSchema::PATHINFO,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
         // 0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE  模式); 3 (兼容模式)  默认为PATHINFO 模式
         'URL_PATHINFO_DEPR'      => '/',    // PATHINFO模式下，各参数之间的分割符号
---
+++ src/Think/Url/UrlCaseSensitivity.php
@@ -0,0 +1,20 @@
+<?php
+    
+    
+    namespace Think\Url;
+
+    /**
+     * Class UrlCaseSensitivity
+     * @package Think\Url
+     */
+    class UrlCaseSensitivity
+    {
+        /**
+         * @const SENSITIVITY 大小写敏感
+         */
+        const SENSITIVITY = true;
+        /**
+         * @const INSENSITIVITY 大小写不敏感
+         */
+        const INSENSITIVITY = false;
+    }
