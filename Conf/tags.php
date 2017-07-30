e1c9af1878edd653c9dcd394108f55bb9df4e038
kwan <303198069@qq.com>
2017-07-31 02:22:37 +0800

    misc

--- Conf/tags.php
+++ Conf/tags.php
@@ -3,27 +3,33 @@
  * 行为定义
  */
 
+use Behavior\ContentReplaceBehavior;
+use Behavior\ParseTemplateBehavior;
+use Behavior\ReadHtmlCacheBehavior;
+use Behavior\ShowPageTraceBehavior;
+use Behavior\WriteHtmlCacheBehavior;
+
 return [
     'app_init'        => [
     ],
     'app_begin'       => [
         // 读取静态缓存
-        \Behavior\ReadHtmlCacheBehavior::class,
+        ReadHtmlCacheBehavior::class,
     ],
     'app_end'         => [
         // 页面Trace显示
-        \Behavior\ShowPageTraceBehavior::class,
+        ShowPageTraceBehavior::class,
     ],
     'view_parse'      => [
         // 模板解析 支持PHP、内置模板引擎和第三方模板引擎
-        \Behavior\ParseTemplateBehavior::class,
+        ParseTemplateBehavior::class,
     ],
     'template_filter' => [
         // 模板输出替换
-        \Behavior\ContentReplaceBehavior::class,
+        ContentReplaceBehavior::class,
     ],
     'view_filter'     => [
         // 写入静态缓存
-        \Behavior\WriteHtmlCacheBehavior::class,
+        WriteHtmlCacheBehavior::class,
     ],
 ];
